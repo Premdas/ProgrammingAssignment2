f5f6b425195010856ed3aee59279c9d677a5825d
Premdas R Nambiar <premdasnambiar@gmail.com>
2014-12-19 19:22:54 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,14 @@
-  #Enter the martix Ex:V<-makeVector(c(4,3,3,2),r,c);r=rows;c=columns; Do not enter singular matrix
+  #Enter the martix Ex:V<-makeVector(c(4,3,3,2),r,c);r=rows;c=columns; If the matrix does not have an inverse it will throw an error
 # This function will return an object which is a list
-makeCacheMatrix <- function(x = numeric(),r,c) {  # input x will be a vector
+makeCacheMatrix <- function(x = numeric(),r,c) # input x will be a vector
+  {  
   k<-matrix(x,r,c)                          # converts it into matrix
   m <- NULL                                 # m is set to NULL everytime makeVector is called
   set <- function(y) {                      # use to set values, not used here
     k <<- y
     m <<- NULL
   }
-  get <- function() k                       # returns the value of the original vector
+  get <- function() k                       # returns the value of the original matrix
   setinverse <- function(inverse) m <<- inverse # used to store the value
   getinverse <- function() m                    # if the inverse is already stored,this function will return the stored value
   list(set = set, get = get,                     # list of internal function
