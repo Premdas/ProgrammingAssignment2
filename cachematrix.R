5fde6643de916fb5b93e07822a4752151b3bbce3
Premdas R Nambiar <premdasnambiar@gmail.com>
2014-12-19 18:58:43 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-  #Enter the martix Ex:V<-makeVector(c(4,3,3,2),r,c);r=rows;c=columns
+  #Enter the martix Ex:V<-makeVector(c(4,3,3,2),r,c);r=rows;c=columns; Do not enter singular matrix
 # This function will return an object which is a list
 makeCacheMatrix <- function(x = numeric(),r,c) {  # input x will be a vector
   k<-matrix(x,r,c)                          # converts it into matrix
@@ -15,7 +15,7 @@ makeCacheMatrix <- function(x = numeric(),r,c) {  # input x will be a vector
        getinverse = getinverse)
 }
 
-# This function will check if the inverse already stored,if its stored then returns inverse else it calculates inverse and returns inverse 
+# This function will check if the inverse is already stored,if its stored then returns inverse else it calculates inverse and returns inverse 
 cacheSolve <- function(k,...) {
   m <- k$getinverse()                      #gets the value of the inverse
   if(!is.null(m)) {                     # checks whether m is NULL
